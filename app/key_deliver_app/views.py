ac98bb1236701c47e4e8e06f1eb743bbbe366a1c
PostMetalFun1992 <kabanovkirill54@gmail.com>
2018-10-03 22:53:07 +0700

    Key deliver logic

--- app/key_deliver_app/views.py
+++ app/key_deliver_app/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
 from key_deliver_app.models import Key
 from key_deliver_app.serializers import KeySerializer
@@ -12,3 +13,10 @@ class KeyList(generics.ListCreateAPIView):
 class KeyDetail(generics.RetrieveUpdateAPIView):
     queryset = Key.objects.all()
     serializer_class = KeySerializer
+
+    def update(self, request, *args, **kwargs):
+        key = self.get_object()
+        key.is_delivered = True
+        key.save()
+
+        return Response(self.get_serializer(key).data)
