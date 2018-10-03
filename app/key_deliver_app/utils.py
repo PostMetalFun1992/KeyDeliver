bc4b7d952bf773dfd19a63378564e5ff6bbfb1f9
PostMetalFun1992 <kabanovkirill54@gmail.com>
2018-10-03 23:01:35 +0700

    Random key generator util

---
+++ app/key_deliver_app/utils.py
@@ -0,0 +1,10 @@
+import string as s
+import random as r
+
+
+DEFAULT_KEY_SIZE = 4
+KEY_PIECES = s.ascii_letters + s.digits
+
+
+def generate_random_value(size=DEFAULT_KEY_SIZE):
+    return ''.join(r.choice(KEY_PIECES) for _ in range(size))
