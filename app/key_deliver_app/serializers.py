115ab8f1c67db7a8bdffa84bb76e64e638a41bb1
PostMetalFun1992 <kabanovkirill54@gmail.com>
2018-10-04 00:08:37 +0700

    Prevent to repay non-delivered keys

--- app/key_deliver_app/serializers.py
+++ app/key_deliver_app/serializers.py
@@ -4,6 +4,14 @@ from key_deliver_app.models import Key
 
 
 class KeySerializer(serializers.ModelSerializer):
+    def validate(self, d):
+        if d['is_repayed'] and not d['is_delivered']:
+            raise serializers.ValidationError(
+                'Cannot repay key before delivery'
+            )
+
+        return d
+
     class Meta:
         model = Key
         fields = ('id', 'value', 'is_delivered', 'is_repayed')
