b19dd3dea6f479cefb2dd045ff25fd998ec3694f
PostMetalFun1992 <kabanovkirill54@gmail.com>
2018-10-03 20:27:50 +0700

    Basic Key model

--- app/key_deliver_app/models.py
+++ app/key_deliver_app/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from django.core.validators import RegexValidator
+
+
+class Key(models.Model):
+    value = models.CharField(
+        max_length=4,
+        unique=True,
+        validators=[
+            RegexValidator(regex='^.{4}$', message='Length must be 4')
+        ]
+    )
+    is_delivered = models.BooleanField(default=False)
+    is_repayed = models.BooleanField(default=False)
