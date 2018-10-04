502836d9476b4ef05f0707b6509776a3de057fe9
PostMetalFun1992 <kabanovkirill54@gmail.com>
2018-10-04 14:24:33 +0700

    Fix unique key generator

--- app/key_deliver_app/utils.py
+++ app/key_deliver_app/utils.py
@@ -1,10 +1,18 @@
 import string as s
 import random as r
 
+from key_deliver_app.models import Key
+
 
 DEFAULT_KEY_SIZE = 4
 KEY_PIECES = s.ascii_letters + s.digits
 
 
-def generate_value(size=DEFAULT_KEY_SIZE):
+def generate_random_value(size=DEFAULT_KEY_SIZE):
     return ''.join(r.choice(KEY_PIECES) for _ in range(size))
+
+
+def generate_unique_key_value():
+    value = generate_random_value()
+    return value if not Key.objects.filter(value=value).exists() \
+        else generate_random_value()
