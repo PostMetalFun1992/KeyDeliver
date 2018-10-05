e5e9e617a0020b7c71b3ed8810c3e4ad64743d26
PostMetalFun1992 <kabanovkirill54@gmail.com>
2018-10-06 00:34:03 +0700

    Move urlconf on project level

--- app/key_deliver/urls.py
+++ app/key_deliver/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from key_deliver_app.views import KeyList, KeyDetail, KeyRepay
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('keys/', include('key_deliver_app.urls')),
+    path('keys/', KeyList.as_view()),
+    path('keys/<int:pk>/', KeyDetail.as_view()),
+    path('keys_repayer/', KeyRepay.as_view()),
 ]
--- app/key_deliver_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from key_deliver_app.views import KeyList, KeyDetail
-
-
-urlpatterns = [
-    path('', KeyList.as_view()),
-    path('<int:pk>/', KeyDetail.as_view()),
-]
